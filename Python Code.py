1714a2dc35e4bbf618aff05306399c2d30423401
georgezervas17 <icsd13055@icsd.aegean.gr>
2018-07-27 18:13:05 +0300

    Find Seasonality Steps
    An introduction in Seasonality terms that include our retail data. It will be helpful if we discover something. It is an overview of the commands that I will use to export the seasonality.

--- Python Code.py
+++ Python Code.py
@@ -1,21 +1,22 @@
 
 import numpy as np #I use numpy for Linear Algebra
 import pandas as pd #I use pandas for data processing, I have to upload my CSV Files
-from prettytable import PrettyTable
+from prettytable import PrettyTable #In Anaconda prompt you have to install the PrettyTable
+									#Command: 'easy_install prettytable'
 
 features = pd.read_csv("features.csv") #Load the 3 CSV that we are gonna
 sales = pd.read_csv("sales.csv")
 stores = pd.read_csv("stores.csv")
-																#In Anaconda prompt you have to install the PrettyTable
-																#Command: 'easy_install prettytable'
-data_inspection = PrettyTable(['Table Name','Table Dimension']) #Use PrettyTable to print the shape of CSV
-data_inspection.add_row(['features',features.shape])
+																
+data_inspection = PrettyTable(['Table Name','Table Dimension'])	#Use PrettyTable to print the shape of CSV 
+data_inspection.add_row(['features',features.shape])			#(Sum of Rows, Columns, Info)
 data_inspection.add_row(['sales',sales.shape])
 data_inspection.add_row(['stores',stores.shape])
-print(data_inspection,'\n')
+print(data_inspection,'\n')																					 
 
 
-print('Features: ', features.columns.tolist()) #Print the Column Names of each CSV
+
+print('Features: ', features.columns.tolist()) 					#Print the Column Names of each CSV
 print('Sales: ',sales.columns.tolist())
 print('Stores: ', stores.columns.tolist())
 
@@ -25,26 +26,26 @@ unified_table = unified_table.merge(stores,how ="left",on=['Store'])				#table s
 unified_table.head()															    #all the values, I use pandas DateFrame
 
 
-import matplotlib.pyplot as plt #import matplotlib for the correlation
+import matplotlib.pyplot as plt 													#import matplotlib for the correlation
 
 
 corrmat = unified_table[['Store','Date','Weekly_Sales','Temperature','Fuel_Price','CPI',
 						 'Unemployment','Type','IsHoliday','MarkDown1',
 						 'MarkDown2','MarkDown3','MarkDown4','MarkDown5',
-						 'Size','Dept']].corr() #I use the unified table to correlate the values
+						 'Size','Dept']].corr() 									#I use the unified table to correlate the values
 
 
-import seaborn as sns #I need this import for the results of the correlation
+import seaborn as sns	#I need this import for the results of the correlation
 
 
 f, ax = plt.subplots(figsize=(12,9))
-sns.heatmap(corrmat,vmax=1, square=True, annot=True ); #We have to change the values in
-													   #because it doesn't correlate those values 
-													   #(Denormalization)
-													   #Temperature,Fuel_Price,CPI,Unenployment
+sns.heatmap(corrmat,vmax=1, square=True, annot=True );	#We have to change the values in
+													    #because it doesn't correlate those values 
+													    #(Denormalization)
+													    #Temperature,Fuel_Price,CPI,Unenployment
 
 
-info = pd.DataFrame(unified_table.dtypes).T.rename(index = {0:'Column Type'}) #Identify how many values are NaN from each column
+info = pd.DataFrame(unified_table.dtypes).T.rename(index = {0:'Column Type'})	#Identify how many values are NaN from each column
 info = info.append(pd.DataFrame(unified_table.isnull().sum()).T.rename(index = {0:'null values (nb)'}))
 info
 
@@ -67,8 +68,8 @@ sns.heatmap(corrmat,vmax=1, square=True, annot=True ); 	#We had ran the Correlat
 # INSIGHT GRAPHS
 												
 top_average_sales= unified_table.groupby(by=['Date'],as_index=False )['Weekly_Sales'].sum() #Sum the Column 'Weekly Sales' by Date
-top_average_sales #Print all values from top_average_sales matrix
-print(top_average_sales.sort_values('Weekly_Sales',ascending= False).head(10)) #TOP 10 DESCENDING (Sorted)
+top_average_sales 																			#Print all values from top_average_sales matrix
+print(top_average_sales.sort_values('Weekly_Sales',ascending= False).head(10)) 				#TOP 10 DESCENDING (Sorted)
 #Date  Weekly_Sales
 #46   2010-12-24   80931415.60
 #98   2011-12-23   76998241.31
@@ -81,7 +82,7 @@ print(top_average_sales.sort_values('Weekly_Sales',ascending= False).head(10)) #
 #113  2012-04-06   53502315.87
 #126  2012-07-06   51253021.88
 
-print(top_average_sales.sort_values('Weekly_Sales',ascending= True).head(10)) #TOP 10 ASCENDING (Sorted)
+print(top_average_sales.sort_values('Weekly_Sales',ascending= True).head(10)) 				#TOP 10 ASCENDING (Sorted)
 # Date  Weekly_Sales
 #51   2011-01-28   39599852.99
 #103  2012-01-27   39834974.67
@@ -94,16 +95,16 @@ print(top_average_sales.sort_values('Weekly_Sales',ascending= True).head(10)) #T
 #86   2011-09-30   42195830.81
 #34   2010-10-01   42239875.87
 
-average_sales_week = unified_table.groupby(by=['Date'], as_index=False)['Weekly_Sales'].sum() #X#RECREATE THE VALUES FOR X AND Y AXIS USING THE PREVIOUS FORMULA
+average_sales_week = unified_table.groupby(by=['Date'], as_index=False)['Weekly_Sales'].sum() #X, RECREATE THE VALUES FOR X AND Y AXIS USING THE PREVIOUS FORMULA
 average_sales = average_sales_week.sort_values('Weekly_Sales',ascending=False)	   #Y
 
 
-plt.figure(figsize=(20,5)) #DEFINE THE PLOT
-plt.plot(average_sales_week.Date,average_sales_week.Weekly_Sales) #IMPORT THE DATA FROM EACH AXIS
+plt.figure(figsize=(20,5))	#DEFINE THE PLOT 
+plt.plot(average_sales_week.Date,average_sales_week.Weekly_Sales)	#IMPORT THE DATA FROM EACH AXIS
 plt.show() #SHOW THE LINE CHART 
 
 
-fig, ax1 = plt.subplots(figsize=(20,5))
+fig, ax1 = plt.subplots(figsize=(20,5)) # 2 Y-AXIS GRAPH COMBINATION OF FUEL_PRICE AND TEMPERATURE (Y),DATE (X)
 ax1.plot(fuel_price.Date,fuel_price.Fuel_Price, 'g-' )
 ax2 = ax1.twinx()
 ax2.plot(temperature.Date,temperature.Temperature, 'b-')
@@ -119,6 +120,35 @@ print(top_sales.sort_values('Weekly_Sales',ascending=False)[:10])
 #  4	 2.995440e+08
 #  14 	 2.889999e+08
 #  13 	 2.865177e+08
+
+
+#DECOMPOSITION THE TIME SERIES
+
+#TRY TO FIND SEASONALITY,TREND,RANDOM IN OUR VALUES
+#https://anomaly.io/seasonal-trend-decomposition-in-r/
+
+# TEMPERATURE = SEASONAL (ADDITIVE TIME SERIES = SEASONAL + TREND + RANDOM)
+# PRICE = (MULTIPLICATIVE TIME SERIES = SEASONAL * TREND * RANDOM )
+
+#1. IMPORT DATA
+
+#2. DETECT THE TREND
+#WE NEED TO FIND THE TREND IN PRICE AND TEMPERATURE DATA
+#WE HAVE TO SMOOTH THE TIME SERIES -> CENTRED MOVING AVERAGE, FOURIER TRANSFORMATION
+
+#3. DETREND THE TIME SERIES
+# THIS ACTION WILL CLEARLY EXPOSES SEASONALITY
+
+#4. AVERAGE THE SEASONALITY
+#WE USE THE DETRENDED TIME SERIES AND ITS EASY AFTER THAT TO COMPUTE THE AVERAGE SEASONALITY.
+#WE HAVE TO DIVIDE BYT HE SEASONALITY PERIOD THAT WE SHOULD (TOTAL WEEKS)
+
+#5. EXAMINING REMAINNING RANDOM NOISE
+#ALL DATA INCLUDE THE NOISE THAT AFFECTS THEM AND MAKE THE RELIABLE.
+
+#6. RECONSTRUC THE ORIGINAL SIGNAL
+
+#7. REVIEW ALL THE GRAPHS (DATA,SEASONAL,TREND,RANDOM)
 
 
 #AUTOCORRELATION INTRODUCTION
