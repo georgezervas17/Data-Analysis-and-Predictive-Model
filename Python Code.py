509f673d472bdcc2fddfcbd253fadf8cc602144b
georgezervas17 <icsd13055@icsd.aegean.gr>
2018-08-09 04:04:18 +0300

    Comment Changes

--- Python Code.py
+++ Python Code.py
@@ -3,49 +3,49 @@ import numpy as np #I use numpy for Linear Algebra
 import pandas as pd #I use pandas for data processing, I have to upload my CSV Files
 from prettytable import PrettyTable #In Anaconda prompt you have to install the PrettyTable
 									#Command: 'easy_install prettytable'
-
-features = pd.read_csv("features.csv") #Load the 3 CSV that we are gonna
+#Load the 3 CSV that we are gonna
+features = pd.read_csv("features.csv") 
 sales = pd.read_csv("sales.csv")
 stores = pd.read_csv("stores.csv")
-																
-data_inspection = PrettyTable(['Table Name','Table Dimension'])	#Use PrettyTable to print the shape of CSV 
-data_inspection.add_row(['features',features.shape])			#(Sum of Rows, Columns, Info)
+
+#Use PrettyTable to print the shape of CSV 
+#(Sum of Rows, Columns, Info)								
+data_inspection = PrettyTable(['Table Name','Table Dimension'])
+data_inspection.add_row(['features',features.shape])			
 data_inspection.add_row(['sales',sales.shape])
 data_inspection.add_row(['stores',stores.shape])
 print(data_inspection,'\n')																					 
 
 
-
-print('Features: ', features.columns.tolist()) 					#Print the Column Names of each CSV
+#Print the Column Names of each CSV
+print('Features: ', features.columns.tolist()) 					
 print('Sales: ',sales.columns.tolist())
 print('Stores: ', stores.columns.tolist())
 
+#Union all the columns in one table so that can easy to correlate all the values, I use pandas DateFrame
+unified_table = sales.merge(features,how="left", on=['Store', 'Date', 'IsHoliday']) 
+unified_table = unified_table.merge(stores,how ="left",on=['Store'])				
+unified_table.head()															    
 
-unified_table = sales.merge(features,how="left", on=['Store', 'Date', 'IsHoliday']) #Union all the columns in one 
-unified_table = unified_table.merge(stores,how ="left",on=['Store'])				#table so that can easy to correlate
-unified_table.head()															    #all the values, I use pandas DateFrame
+#import matplotlib for the correlation
+import matplotlib.pyplot as plt 													
 
-
-import matplotlib.pyplot as plt 													#import matplotlib for the correlation
-
-
+#I use the unified table to correlate the values
 corrmat = unified_table[['Store','Date','Weekly_Sales','Temperature','Fuel_Price','CPI',
 						 'Unemployment','Type','IsHoliday','MarkDown1',
 						 'MarkDown2','MarkDown3','MarkDown4','MarkDown5',
-						 'Size','Dept']].corr() 									#I use the unified table to correlate the values
+						 'Size','Dept']].corr() 									
 
+#I need this import for the results of the correlation
+import seaborn as sns	
 
-import seaborn as sns	#I need this import for the results of the correlation
-
-
+#We have to change the values in because it doesn't correlate those values
+#(Denormalization) Temperature,Fuel_Price,CPI,Unenployment
 f, ax = plt.subplots(figsize=(12,9))
-sns.heatmap(corrmat,vmax=1, square=True, annot=True );	#We have to change the values in
-													    #because it doesn't correlate those values 
-													    #(Denormalization)
-													    #Temperature,Fuel_Price,CPI,Unenployment
+sns.heatmap(corrmat,vmax=1, square=True, annot=True );	
 
-
-info = pd.DataFrame(unified_table.dtypes).T.rename(index = {0:'Column Type'})	#Identify how many values are NaN from each column
+#Identify how many values are NaN from each column
+info = pd.DataFrame(unified_table.dtypes).T.rename(index = {0:'Column Type'})	
 info = info.append(pd.DataFrame(unified_table.isnull().sum()).T.rename(index = {0:'null values (nb)'}))
 info
 
@@ -53,23 +53,26 @@ info
 unified_table.fillna(0, inplace=True)
 
 
-
+#I use the unified table to correlate all the values
 corrmat = unified_table[['Store','Date','Weekly_Sales','Temperature','Fuel_Price','CPI',
 						 'Unemployment','Type','IsHoliday','MarkDown1',
 						 'MarkDown2','MarkDown3','MarkDown4','MarkDown5',
-						 'Size','Dept']].corr() 						#I use the unified table to correlate all the values
+						 'Size','Dept']].corr() 						
 
 
-
+	#We had ran the Correlation and it's time to see the matrix with the results.			
 f, ax = plt.subplots(figsize=(12,9)) 
-sns.heatmap(corrmat,vmax=1, square=True, annot=True ); 	#We had ran the Correlation and it's time to see the 
-														#the matrix with the results.				 
+sns.heatmap(corrmat,vmax=1, square=True, annot=True ); 
+														 
 
 # INSIGHT GRAPHS
-												
-top_average_sales= unified_table.groupby(by=['Date'],as_index=False )['Weekly_Sales'].sum() #Sum the Column 'Weekly Sales' by Date
-top_average_sales 																			#Print all values from top_average_sales matrix
-print(top_average_sales.sort_values('Weekly_Sales',ascending= False).head(10)) 				#TOP 10 DESCENDING (Sorted)
+
+#Sum the Column 'Weekly Sales' by Date
+#Print all values from top_average_sales matrix
+#TOP 10 DESCENDING (Sorted)
+top_average_sales= unified_table.groupby(by=['Date'],as_index=False )['Weekly_Sales'].sum() 
+																	
+print(top_average_sales.sort_values('Weekly_Sales',ascending= False).head(10)) 				
 #Date  Weekly_Sales
 #46   2010-12-24   80931415.60
 #98   2011-12-23   76998241.31
@@ -82,7 +85,8 @@ print(top_average_sales.sort_values('Weekly_Sales',ascending= False).head(10)) 	
 #113  2012-04-06   53502315.87
 #126  2012-07-06   51253021.88
 
-print(top_average_sales.sort_values('Weekly_Sales',ascending= True).head(10)) 				#TOP 10 ASCENDING (Sorted)
+#TOP 10 ASCENDING (Sorted)
+print(top_average_sales.sort_values('Weekly_Sales',ascending= True).head(10)) 				
 # Date  Weekly_Sales
 #51   2011-01-28   39599852.99
 #103  2012-01-27   39834974.67
@@ -95,22 +99,29 @@ print(top_average_sales.sort_values('Weekly_Sales',ascending= True).head(10)) 		
 #86   2011-09-30   42195830.81
 #34   2010-10-01   42239875.87
 
-average_sales_week = unified_table.groupby(by=['Date'], as_index=False)['Weekly_Sales'].sum() #X, RECREATE THE VALUES FOR X AND Y AXIS USING THE PREVIOUS FORMULA
-average_sales = average_sales_week.sort_values('Weekly_Sales',ascending=False)	   #Y
+#X, RECREATE THE VALUES FOR X AND Y AXIS USING THE PREVIOUS FORMULA
+average_sales_week = unified_table.groupby(by=['Date'], as_index=False)['Weekly_Sales'].sum() 
+#Y
+average_sales = average_sales_week.sort_values('Weekly_Sales',ascending=False)	   
+
+#DEFINE THE PLOT 
+#IMPORT THE DATA FROM EACH AXIS
+#SHOW THE LINE CHART 
+plt.figure(figsize=(20,5))
+plt.plot(average_sales_week.Date,average_sales_week.Weekly_Sales)	
+plt.show()
 
 
-plt.figure(figsize=(20,5))	#DEFINE THE PLOT 
-plt.plot(average_sales_week.Date,average_sales_week.Weekly_Sales)	#IMPORT THE DATA FROM EACH AXIS
-plt.show() #SHOW THE LINE CHART 
-
-
+#From unified_table we need to measure the mean values of fuel_price and temperatures.
 fuel_price = unified_table.groupby(by=['Date'], as_index=False)['Fuel_Price'].mean()
 temperature = unified_table.groupby(by=['Date'], as_index=False)['Temperature'].mean()
 
-fig, ax1 = plt.subplots(figsize=(5,5)) # 2 Y-AXIS GRAPH COMBINATION OF FUEL_PRICE AND TEMPERATURE (Y),DATE (X)
+# 2 Y-AXIS GRAPH COMBINATION OF FUEL_PRICE AND TEMPERATURE (Y),DATE (X)
+fig, ax1 = plt.subplots(figsize=(5,5)) 
 ax1.plot(fuel_price.Date,fuel_price.Fuel_Price, 'g-' )
 ax2 = ax1.twinx()
-ax2.plot(temperature.Date,temperature.Temperature, 'b-') #SHOW US THE SEASONALITY
+#SHOW US THE SEASONALITY
+ax2.plot(temperature.Date,temperature.Temperature, 'b-')
 plt.show() 
 
 
@@ -129,8 +140,6 @@ print(top_sales.sort_values('Weekly_Sales',ascending=False)[:10])
 #
 #DECOMPOSITION THE TIME SERIES INTO 3 COMPONENTS
 #
-
-
 
 #TRY TO FIND SEASONALITY,TREND,RANDOM IN OUR VALUES
 #https://anomaly.io/seasonal-trend-decomposition-in-r/
@@ -160,35 +169,18 @@ print(top_sales.sort_values('Weekly_Sales',ascending=False)[:10])
 
 
 
-#
-#REGRESSION
-#
-
-#SCATTER PLOT FUELPRICE AND TEMPERATURE
-colors = np.random.rand(len(fuel_price))
-area = (120 * np.random.rand(len(fuel_price)))**2  # 0 to 15 point radii
-plt.scatter(fuel_price.Fuel_Price,temperature.Temperature,c = colors, alpha =0.6)
-
-
-p1 = np.polyfit(fuel_price.Fuel_Price,temperature.Temperature,1) #SLOPE AND INTERCEPT
-print(p1)
-
-
-from matplotlib.pyplot import *
-plot(fuel_price.Fuel_Price,temperature.Temperature,'o')
-plot(fuel_price.Fuel_Price,np.polyval(p1,fuel_price.Fuel_Price),'-r')  #LINEAR FIT INTO FUEL PRICE AND TEMPERATURE
 
 #
 #AUTOCORRELATION = SERIAL CORRELATION INTRODUCTION
 #
 
 
-#Our problem fitting with Autocorrelation
-
+#Our problem fitting with Autocorrelation (Time Series Problem)
 # Create the first Forecast Model for Total Sales Volume.
 
 
-from statsmodels.graphics.tsaplots import acf,pacf,plot_acf,plot_pacf #IMPORT THE NECESSARY LIBRARY FOR THE AUTOCORRELATION AND PARTIAL
+#IMPORT THE NECESSARY LIBRARY FOR THE AUTOCORRELATION AND PARTIAL
+from statsmodels.graphics.tsaplots import acf,pacf,plot_acf,plot_pacf 
 #acf = autocorrelation function
 #pacf = partial autocorrelation function
 #See the Table Dimension
@@ -206,7 +198,8 @@ day_i_minus = x[:-1]
 
 #The number that measures how correlate are the day_i with the day_i_minus
 np.corrcoef(day_i_minus,day_i)[0,1]
-#0.33
+#0.3377879144700981
+
 
 #I present you in a scatter plot the distribution of avg sales of day i (x) and avg sales day i-1 
 #With the diagram we understand that i days and the day before are highly correlated.
@@ -238,3 +231,27 @@ plt.show()
 #1,52 WEEK IS POSITIVE CORRELATED
 #6 WEEK IS NEGATIVE CORRELATED
 
+
+
+
+
+
+
+
+#
+#REGRESSION ANALYSIS
+#
+
+#SCATTER PLOT FUELPRICE AND TEMPERATURE
+colors = np.random.rand(len(fuel_price))
+area = (120 * np.random.rand(len(fuel_price)))**2  # 0 to 15 point radii
+plt.scatter(fuel_price.Fuel_Price,temperature.Temperature,c = colors, alpha =0.6)
+
+#SLOPE AND INTERCEPT
+p1 = np.polyfit(fuel_price.Fuel_Price,temperature.Temperature,1) 
+print(p1)
+
+#LINEAR FIT INTO FUEL PRICE AND TEMPERATURE
+from matplotlib.pyplot import *
+plot(fuel_price.Fuel_Price,temperature.Temperature,'o')
+plot(fuel_price.Fuel_Price,np.polyval(p1,fuel_price.Fuel_Price),'-r')  
